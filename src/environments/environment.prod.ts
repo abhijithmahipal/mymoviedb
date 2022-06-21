4c5c51625466ce7c8ad62c79b4115b5fb8250864
abhijithmahipal <abhijithmahipal@gmail.com>
2022-06-21 15:00:43 +0530

    added configuration in prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDfcBQGMNcblVMa0O2yutil_5L38ZjSKVE',
+    authDomain: 'mymoviedb-8f172.firebaseapp.com',
+    projectId: 'mymoviedb-8f172',
+    storageBucket: 'mymoviedb-8f172.appspot.com',
+    messagingSenderId: '1022756529442',
+    appId: '1:1022756529442:web:c737f9e289d6a05726717b',
+    measurementId: 'G-WHGRT6P8R1',
+  },
 };
